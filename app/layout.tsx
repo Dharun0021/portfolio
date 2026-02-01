00cd07ff4fafc8a572f191ae13f35b13cf53bbcd
DHARUN KUMAR K <162271954+Dharun0021@users.noreply.github.com>
2026-02-01 10:56:42 +0530

    favicon add

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,20 +19,7 @@ export const metadata: Metadata = {
   description: 'Software Developer with 1.5+ years of hands-on industry experience in designing, developing, and maintaining secure, data-driven applications and dashboards for real-world business needs.',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/favicon.svg',
     apple: '/apple-icon.png',
   },
 }
